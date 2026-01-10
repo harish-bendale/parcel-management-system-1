d157e9b3eb5c2b1b5c378a47bff62ad4cc912833
Harshit-Bafna <bafnaharshit7891@gmail.com>
2026-01-10 16:18:15 +0530

    feat: Spring boot project setup

---
+++ src/main/java/com/parcel_managemant_system/app/ParcelManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.parcel_managemant_system.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcelManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parcel_managemant_system/app/ParcelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcel_managemant_system.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcelManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
