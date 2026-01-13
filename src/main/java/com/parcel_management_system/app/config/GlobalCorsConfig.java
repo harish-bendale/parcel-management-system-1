a92f9fde46c08168a30d904f52e3b0762b9e730d
harish-bendale <harishbendale80@gmail.com>
2026-01-13 23:56:40 +0530

    Added cors

---
+++ src/main/java/com/parcel_management_system/app/config/GlobalCorsConfig.java
@@ -0,0 +1,18 @@
+package com.parcel_management_system.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") // Angular default port
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
